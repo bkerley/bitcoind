278802de909f8e9f773a054ffb20a2b0215ebe5e
Bryce Kerley <bkerley@brycekerley.net>
2011-04-16 21:56:32 -0400

    test start

---
+++ test/balance_test.rb
@@ -0,0 +1,19 @@
+require 'helper'
+
+class BalanceTest < Test::Unit::TestCase
+  context 'balance operation' do
+    setup do
+      RestClient = stub 'RestClient' do
+        expects(:post).
+          with('http://testuser:testpass@localhost:8332',{'method'=>'getbalance', 'id'=>'jsonrpc'}.to_json).
+          returns("{\"result\":12.34000000,\"error\":null,\"id\":\"jsonrpc\"}\n")
+      end
+      @bcd = Bitcoind.new 'testuser', 'testpass'
+    end
+
+    should 'get the balance' do
+      @result = @bcd.balance
+      assert_equal 12.34, @result
+    end
+  end
+end
---
+++ test/helper.rb
@@ -0,0 +1,3 @@
+require 'shoulda'
+require 'mocha'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'bitcoind')
